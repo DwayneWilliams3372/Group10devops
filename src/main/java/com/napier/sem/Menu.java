06c66e87f401ce873c259b798e05e9fff589dc59
Chan Myae <cm27102003@gmail.com>
2025-11-13 18:09:59 +0700

    Adding City related Report

--- src/main/java/com/napier/sem/Menu.java
+++ src/main/java/com/napier/sem/Menu.java
@@ -8,11 +8,13 @@ import java.util.Scanner;
 public class Menu {
     private final Connection con;
     private final Country country;
+    private final City city;
     private final Scanner scanner = new Scanner(System.in);
 
     public Menu(Connection con) {
         this.con = con;
         this.country = new Country(con);
+        this.city = new City(con);
     }
 
     public void start() {
@@ -24,6 +26,8 @@ public class Menu {
             System.out.println("4. Top N countries in the world");
             System.out.println("5. Top N countries in a continent");
             System.out.println("6. Top N countries in a region");
+            System.out.println("7. All the cities in the world");
+            System.out.println("8. All the cities in a continent");
             System.out.println("0. Exit");
             System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
@@ -74,6 +78,19 @@ public class Menu {
                     country.printCountries(topCountriesInRegion);
                     break;
                 }
+                case 7: {
+                    ArrayList<City> cities = city.getCities();
+                    city.printCities(cities);
+                    break;
+                }
+                case 8: {
+                    System.out.print("Enter continent: ");
+                    String cont = scanner.nextLine();
+                    ArrayList<City> continentCities = city.getCountriesContinent(cont);
+                    city.printCities(continentCities);
+                    break;
+                }
+
                 case 0:  {
                     System.out.println("Exiting menu...");
                     return;

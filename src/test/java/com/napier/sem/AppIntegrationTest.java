8864cd30cac5fb729140c7e909481b6a7c526b63
Chan Myae <cm27102003@gmail.com>
2025-11-24 07:33:34 +0700

    Integration test update

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -1,7 +1,9 @@
 package com.napier.sem;
 
 import org.junit.jupiter.api.*;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.*;
 import java.util.ArrayList;
 
